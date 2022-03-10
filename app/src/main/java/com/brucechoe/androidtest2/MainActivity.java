cdf4bdabd6c2ec5686d9abe6d74360f150b5cde9
Dev-Choi <>
2022-03-10 16:27:47 +0900

    Initial commit

---
+++ app/src/main/java/com/brucechoe/androidtest2/MainActivity.java
@@ -0,0 +1,32 @@
+package com.brucechoe.androidtest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button showGuess;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showGuess = findViewById(R.id.button_guess);
+
+        showGuess.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, ShowGuess.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+}
---
+++ app/src/main/java/com/brucechoe/androidtest2/ShowGuess.java
@@ -0,0 +1,14 @@
+package com.brucechoe.androidtest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ShowGuess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_guess);
+    }
+}
